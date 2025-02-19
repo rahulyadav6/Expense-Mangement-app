ded01516dce452ffee36d981ed95b45a23ea2dbf
Rahul Kumar Yadav <ry573870@gmail.com>
2025-02-19 10:50:30 +0530

    404 Not found page updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Signuppage} from "./components/Signuppage"
 import { Layout } from "./components/Layout"
 import {Loginpage} from './components/Loginpage'
 import { Landingpage } from "./components/Landingpage"
-import { ErrorPage } from "./components/ErrorPage"
+import { NotFoundPage } from "./components/NotFoundPage"
 
 // import { useState } from 'react'
 function App() {
@@ -16,7 +16,7 @@ function App() {
           <Route path="/"  element={<Landingpage />} />
           <Route path="/signup" element={<Signuppage />} />
           <Route path="/signin" element={<Loginpage />} />
-          <Route path="/*" element={<ErrorPage />} />
+          <Route path="/*" element={<NotFoundPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- frontend/src/components/ErrorPage.jsx
+++
@@ -1,7 +0,0 @@
-export function ErrorPage(){
-    return(
-      <div>
-        Sorry page not found
-      </div>
-    );
-  }
