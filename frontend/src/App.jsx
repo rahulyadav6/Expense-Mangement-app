5796e29bba2479565a1004c25cf231faa9377de7
Rahul Kumar Yadav <ry573870@gmail.com>
2025-03-01 12:47:23 +0530

    signup page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Layout } from "./components/Layout"
 import LandingPage from "./components/LandingPage"
+import Signup from "./components/Signup"
 
 
 // import { useState } from 'react'
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
           <Route path="/"  element={<LandingPage />} />
+          <Route path="/signup" element={<Signup />} />
           {/* <Route path="/*" element={<NotFoundPage />} /> */}
           </Route>
         </Routes>
--- frontend/src/components/Landingpage.jsx
+++ frontend/src/components/Landingpage.jsx
@@ -5,8 +5,8 @@ const LandingPage = () => {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-200">
       <main className="max-w-7xl mx-auto px-4 py-20">
-        <div className="text-center mb-20">
-          <h1 className="text-6xl font-bold mb-6">
+        <div className="text-center mb-64">
+          <h1 className="text-6xl font-bold mb-6 mt-10">
             <span className="bg-gradient-to-r from-blue-600 to-green-600 bg-clip-text text-transparent">
               Take Control
             </span>
