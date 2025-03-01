ae47e7e726a2c6550337ed6c690a4823f009f894
Rahul Kumar Yadav <ry573870@gmail.com>
2025-03-01 12:50:03 +0530

    signin page started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Layout } from "./components/Layout"
 import LandingPage from "./components/LandingPage"
 import Signup from "./components/Signup"
+import Signin from "./components/Signin"
 
 
 // import { useState } from 'react'
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Layout />}>
           <Route path="/"  element={<LandingPage />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
           {/* <Route path="/*" element={<NotFoundPage />} /> */}
           </Route>
         </Routes>
